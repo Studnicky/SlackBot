b1d897d7fbee25d69ad7ec1467ae86aebe19e7e1
Andrew Studnicky <a.j.studnicky@gmail.com>
2016-05-12 22:35:13 -0400

    WIP

--- app/slack/meetup.js
+++ app/slack/meetup.js
@@ -7,18 +7,18 @@ if(process.env.MEETUP_API_KEY){
 }
 
 var config = [
-'Design-with-a-Purpose',
-'familab',
-'OrlandoDevs',
-'Orlando-Tech',
-'Code-For-Orlando'
+	'Design-with-a-Purpose',
+	'familab',
+	'OrlandoDevs',
+	'Orlando-Tech',
+	'Code-For-Orlando'
 ];
 var colors = [
-'#7F1C1B',
-'#AC2624',
-'#AC2624',
-'#E53330',
-'#FF3835'
+	'#7F1C1B',
+	'#AC2624',
+	'#AC2624',
+	'#E53330',
+	'#FF3835'
 ];
 
 var meetup = {
@@ -26,15 +26,50 @@ var meetup = {
 	events: function(controller, bot){
 
 		//	Show from a specific meetup by name
-		controller.hears(['(find|get|list|show) meetup[s]? (.*)'], ['direct_message','direct_mention','mention'], function(bot,message) {
+		controller.hears(['[find|get|list|show] meetup[s]? (.*)'], ['direct_message','direct_mention','mention'], function(bot,message) {
 			//	Show typing indicator while API fetches
 			bot.reply(message, {"type": "typing"});
 			//	Get the important information...
-			var query = message.match[2];
+			var query = message.match[1];
 			var URI = 'https://api.meetup.com/' + query + '/events?&sign=true&page=5';
 			//	Call meetup API to find 
 			request(URI, function (error, response, body) {
 				if (!error && response.statusCode == 200) {
+					//	Meetups are contained in body
+					var meetups = JSON.parse(body);
+					var attachments = parseEvents(meetups);
+
+					var post = {
+						channel: message.channel,
+						username: 'Meetup',
+						icon_emoji: ':meetup:'
+					};
+					if (attachments.length > 0){
+						post.attachments = JSON.stringify(attachments);
+					} else {
+						post.text = "No meetups found!";
+					}
+					bot.api.chat.postMessage(post, function(err, response) {
+						if(err){ return console.log(err); };
+					});
+				}
+			});
+		});
+
+		//	Show from a specific meetup by name
+		controller.hears(['[find|get|list|show] meetup[s]? [about|for] (.*) [near|in] (.*)'], ['direct_message','direct_mention','mention'], function(bot,message) {
+			//	Show typing indicator while API fetches
+			bot.reply(message, {"type": "typing"});
+			//	Get the important information...
+			var query = message.match[1];
+			var city = message.match[2];
+			var URI = 'https://api.meetup.com/2/open_events?photo-host=public&topic=' + query + '&city=' + city + '&key=' + meetup_api_key + '&sign=true&page=5';
+			//	Call meetup API to find 
+			request(URI, function (error, response, body) {
+				if (!error && response.statusCode == 200) {
+
+					console.log(body);
+
 					//	Meetups are contained in body
 					var meetups = JSON.parse(body);
 					var attachments = parseEvents(meetups);
@@ -54,16 +89,13 @@ var meetup = {
 				}
 			});
 		});
-
-	//	https://api.meetup.com/2/open_events?photo-host=public&topic=programming&city=orlando&sign=true&page=5
-
 	}
 }
 
 function parseEvents(meetups){
-	var meetups = [];
+	var attachments = [];
 	//	Define array of attachments for each event...
-	events.map(function(event, i){
+	meetups.map(function(event, i){
 		meetup = {	//	These fields are required by meetup and should always be here
 			'fallback': event.name,
 			'color': colors[i],
@@ -71,9 +103,9 @@ function parseEvents(meetups){
 			'title_link': event.link,
 			'fields': parseFields(event)
 		};
-		return meetups.push(meetup);
+		return attachments.push(meetup);
 	});
-	return meetups;
+	return attachments;
 }
 
 function parseFields(event){
