023db8c492f7c3821c19e1a85a47b20bf8fdfa67
Andrew Studnicky <a.j.studnicky@gmail.com>
2016-05-12 22:08:43 -0400

    Meetup search by group

--- app/slack/meetup.js
+++ app/slack/meetup.js
@@ -1,6 +1,10 @@
 var request = require('request');
 
-var meetup_api_key = process.env.MEETUP_API_KEY;
+if(process.env.MEETUP_API_KEY){
+	var meetup_api_key = process.env.MEETUP_API_KEY;
+} else {
+	console.log("** No Meetup API key in .env!");
+}
 
 var config = [
 'Design-with-a-Purpose',
@@ -9,67 +13,95 @@ var config = [
 'Orlando-Tech',
 'Code-For-Orlando'
 ];
+var colors = [
+'#7F1C1B',
+'#AC2624',
+'#AC2624',
+'#E53330',
+'#FF3835'
+];
 
 var meetup = {
 	name: 'meetup',
 	events: function(controller, bot){
 
 		//	Show from a specific meetup by name
-		controller.hears(['(find|get|list|show) meetup (.*)'], ['direct_message','direct_mention','mention'], function(bot,message) {
-
-			bot.reply(message, {"type": "typing"});	//	Show typing indicator while API fetches
-
-			var query = 'https://api.meetup.com/' + message.match[2] + '/events?&sign=true&page=5';
-
+		controller.hears(['(find|get|list|show) meetup[s]? (.*)'], ['direct_message','direct_mention','mention'], function(bot,message) {
+			//	Show typing indicator while API fetches
+			bot.reply(message, {"type": "typing"});
+			//	Get the important information...
+			var query = message.match[2];
+			var URI = 'https://api.meetup.com/' + query + '/events?&sign=true&page=5';
 			//	Call meetup API to find 
-			request(query, function (error, response, body) {
+			request(URI, function (error, response, body) {
 				if (!error && response.statusCode == 200) {
-
-					var events = JSON.parse(body);
-					var event_fields = [];
-
-					for (var event in events) {
-						event_fields.push({
-							"title": events[event].name || "",
-							"value": events[event].link || "",
-							"short": false
-						});
-						if(events[event].venue){
-							event_fields.push({
-								"title": "Address:",
-								"value": events[event].venue.address_1 || "",
-								"short": true
-							});
-						}
-						event_fields.push({
-							"title": "Time:",
-							"value": new Date(events[event].time) || "",
-							"short": true
-						});
-					}
-					var meetups = [{
-						"fallback": "Meetups from " + message.match[2],
-						"color": "#AC2624",
-						"title": "Meetups from " + message.match[2],
-						"fields": event_fields,
-					}];
-
+					//	Meetups are contained in body
+					var meetups = JSON.parse(body);
+					var attachments = parseEvents(meetups);
 					var post = {
 						channel: message.channel,
 						username: 'Meetup',
-						icon_emoji: ':meetup:',
-						attachments: JSON.stringify(meetups),
-						text: ' '
+						icon_emoji: ':meetup:'
 					};
-
+					if (attachments.length > 0){
+						post.attachments = JSON.stringify(attachments);
+					} else {
+						post.text = "No meetups found!";
+					}
 					bot.api.chat.postMessage(post, function(err, response) {
 						if(err){ return console.log(err); };
 					});
-
 				}
 			});
 		});
+
+	//	https://api.meetup.com/2/open_events?photo-host=public&topic=programming&city=orlando&sign=true&page=5
+
 	}
+}
+
+function parseEvents(meetups){
+	var meetups = [];
+	//	Define array of attachments for each event...
+	events.map(function(event, i){
+		meetup = {	//	These fields are required by meetup and should always be here
+			'fallback': event.name,
+			'color': colors[i],
+			'title': event.group.name + ': ' + event.name,
+			'title_link': event.link,
+			'fields': parseFields(event)
+		};
+		return meetups.push(meetup);
+	});
+	return meetups;
+}
+
+function parseFields(event){
+	var fields = [];
+	//	Make attachment fields... These are optional, use with caution!
+	if(event.venue){
+		fields.push({
+			'fallback': event.venue.name || '',
+			'title': 'Location',
+			'value': (event.venue.name || '') + '\n' + (event.venue.address_1 || ''),
+			"short": false
+		});
+	}
+	if(event.time){
+		fields.push({
+			'fallback': new Date(event.time).toDateString() || '',
+			'title': 'Date',
+			'value': new Date(event.time).toDateString() || '',
+			"short": true
+		});
+		fields.push({
+			'fallback': new Date(event.time).toTimeString() || '',
+			'title': 'Time',
+			'value': new Date(event.time).toTimeString() || '',
+			"short": true
+		});
+	}
+	return fields;
 }
 
 module.exports = meetup;
